ea1a02cbc3e3c3fe6ee574763b39a73fd2f7656d
Minnowfeather <35814311+Minnowfeather@users.noreply.github.com>
2020-09-06 14:00:24 -0700

    Added setChannel command

--- bot.py
+++ bot.py
@@ -10,6 +10,8 @@ tokenReader = open("token.txt", "r")
 token = tokenReader.read()
 
 
+
+
 client = discord.Client()
 @client.event
 async def on_ready():
@@ -20,16 +22,17 @@ async def on_ready():
 egging = False
 prefix = '!'
 approved_egging_words = ['egg', 'huevo', 'tamago', 'たまご', '卵']
+eggChannel = None
 
-def stopEgging():
+def deltaEgg():
 	global egging
-	print("Egging stopped")
 	egging = False
-
-
+	print("Egg stopped")
+	
+	
 @client.event
 async def on_message(message):
-	global egging, prefix, approved_egging_words
+	global egging, prefix, approved_egging_words, eggChannel
 	if message.author == client.user:
 		return
 	
@@ -42,24 +45,30 @@ async def on_message(message):
 	if msg[0] in approved_egging_words:
 		if egging:
 			return
+		if eggChannel == None:
+			await message.channel.send("Egg channel has not been set up! Use `" + prefix + "setChannel` to set up")
+			return
 		# elif len(message.author.voice.channel) >= 2:
 		mav = message.author.voice
-		if mav != None and message.channel == client.get_channel(671538516005748750):
+		if mav != None:
 			if len(mav.channel.members) >= 2:
 				egging = True
-				t = Timer(10.0, stopEgging)
+				t = Timer(10.0, deltaEgg)
 				t.start()
-				print(message.author.display_name + " started un huevo.")
+				print(message.author.display_name + " started a " + msg[0] + ".") 
+	elif msg[0] == "setChannel":
+		eggChannel = message.channel.id
+		await message.channel.send("Egg channel set!")
 
 
 @client.event
 async def on_voice_state_update(member, before, after):
-	global egging, members_in_vc 
+	global egging, members_in_vc, eggChannel
 	if egging:
 		if after.channel == None:
 			channelPeople = before.channel.members
 			if len(channelPeople) == 1:
-				channel = client.get_channel(671538516005748750)
+				channel = client.get_channel(eggChannel)
 				await channel.send("{} egg" .format(channelPeople[0].mention)) 
 				egging = False
 
